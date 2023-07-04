e1aff384db4e7050a81bdfd26cce33ea2d9a25ef
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2023-07-04 13:42:05 -0300

    criando as classes criando a Interface IGenericDAO e fazendo as sobrecargas e o implements nas respectivas classes, fazendo insert,remover,busca da cidade implementando com banco de dados, ajustando alguns menus no main e criando o remover Cidade.

--- src/repository/CidadeDAO.java
+++ src/repository/CidadeDAO.java
@@ -1,11 +1,16 @@
 package repository;
 
+import com.mysql.cj.xdevapi.Client;
+import model.Cidade;
 import model.EstadoEnum;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CidadeDAO {
+public final class CidadeDAO implements IGenericDAO<Cidade> {
+
+    List<Cidade> cidades = new ArrayList<>();
 
 
     public static Object[] findEstadoUFInArray() {
@@ -18,6 +23,16 @@ public class CidadeDAO {
     return estadoUf.toArray();
     }
 
+    public static List<EstadoEnum> buscaEstadoEnumBancoDados(Integer posicaoEstado) {
+        List<EstadoEnum> estadoUfFiltradas = new ArrayList<>();
+        for (EstadoEnum estadoEnum : EstadoEnum.values()) {
+            if (estadoEnum.ordinal() == posicaoEstado){
+                estadoUfFiltradas.add(estadoEnum);
+            }
+        }
+        return estadoUfFiltradas;
+    }
+
     public static List<EstadoEnum> buscarPorNome(Object nome) {
         List<EstadoEnum> estadoUfFiltradas = new ArrayList<>();
         for (EstadoEnum estadoEnum : EstadoEnum.values()) {
@@ -27,4 +42,59 @@ public class CidadeDAO {
         }
         return estadoUfFiltradas;
     }
+
+    public Object[] findCidadeInArray() {
+        List<Cidade> cidade = buscarTodos();
+        List<String> cidadeNomes = new ArrayList<>();
+
+        for (Cidade cidade1 : cidade) {
+           cidadeNomes.add(cidade1.getNome());
+        }
+
+        return cidadeNomes.toArray();
+    }
+
+    @Override
+    public void salvar(Cidade cidade) {
+        CidadeRepository repository =  new CidadeRepository();
+
+        try {
+            repository.insere(cidade);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void remover(Cidade cidade) throws SQLException, ClassNotFoundException {
+        CidadeRepository cidadeRepository = new CidadeRepository();
+        cidadeRepository.delete(cidade);
+
+    }
+
+    @Override
+    public List<Cidade> buscarPorNome(String nome) {
+        List<Cidade> cidade = buscarTodos();
+        List<Cidade> filtradas = new ArrayList<>();
+
+        for (Cidade cidade1 : cidade) {
+            if (cidade1.getNome().contains(nome)) {
+                filtradas.add(cidade1);
+            }
+        }
+        return filtradas;
+    }
+
+    @Override
+    public List<Cidade> buscarTodos() {
+        CidadeRepository cidadeRepository = new CidadeRepository();
+        try {
+            cidades = cidadeRepository.busca();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return cidades;
+    }
 }
+
